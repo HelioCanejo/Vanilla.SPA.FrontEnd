b1970fce996a60f8495042a9f967e04adcd79f6a
HelioCanejo <heliocg93@gmail.com>
2023-12-01 20:33:11 -0300

    2023-12-01

--- pages/new/new.js
+++ pages/new/new.js
@@ -1,38 +1,58 @@
+// Inicializa uma string com uma opção padrão para o dono (owner) no formato de um elemento de opção HTML.
 var ownerOptions = '<option value="">-- Selecione --</option>';
 
+// Função principal chamada quando o documento HTML está pronto.
 function myHome() {
+    // Muda o título para 'Novo Documento'.
     changeTitle('Novo Documento');
+
+    // Obtém as opções de donos para preencher o menu suspenso.
     getOwnersToSelect();
+
+    // Verifica se há uma guia aberta na sessão e a exibe.
     if (sessionStorage.openTab == undefined)
-        sessionStorage.openTab = 'item'
+        sessionStorage.openTab = 'item';
     showTab(sessionStorage.openTab);
-    $('#btnNewOwner').click(() => { showTab('owner') });
+
+    // Adiciona eventos de clique aos botões para mostrar as guias de dono (owner) e item.
+    $('#btnNewOwner').click(() => { showTab('owner'); });
     $('#btnNewItem').click(() => { showTab('item'); });
+
+    // Adiciona um evento de envio de formulário para processar e enviar dados.
     $('.tabs form').submit(sendData);
 }
 
+// Função para processar e enviar dados do formulário.
 function sendData(ev) {
     ev.preventDefault();
 
+    // Inicializa um objeto JSON vazio.
     var formJSON = {};
+
+    // Obtém os dados do formulário e os adiciona ao objeto JSON, removendo tags HTML.
     const formData = new FormData(ev.target);
     formData.forEach((value, key) => {
         formJSON[key] = stripTags(value);
         $('#' + key).val(formJSON[key]);
     });
 
+    // Verifica se algum campo está vazio e retorna false se houver.
     for (const key in formJSON)
         if (formJSON[key] == '')
             return false;
 
+    // Chama a função para salvar os dados.
     saveData(formJSON);
     return false;
 }
 
+// Função para salvar dados no servidor.
 function saveData(formJSON) {
-    requestURL = `${app.apiBaseURL}/${formJSON.type}s`;
+    // Constrói a URL da solicitação com base no tipo de formulário.
+    var requestURL = `${app.apiBaseURL}/${formJSON.type}s`;
     delete formJSON.type;
 
+    // Modifica propriedades para garantir consistência no envio de dados.
     if (formJSON.ownerName != undefined) {
         formJSON['name'] = formJSON.ownerName;
         delete formJSON.ownerName;
@@ -43,7 +63,7 @@ function saveData(formJSON) {
         delete formJSON.itemName;
     }
 
-    // jQuery: acessa a API usando AJAX.
+    // Faz uma solicitação AJAX usando jQuery para enviar dados ao servidor.
     $.ajax({
         type: "POST",
         url: requestURL,
@@ -52,7 +72,8 @@ function saveData(formJSON) {
         dataType: "json"
     })
         .done(() => {
-            viewHTML = `
+            // Se bem-sucedido, atualiza o conteúdo da guia com uma mensagem de sucesso.
+            var viewHTML = `
                 <form>
                     <h3>Oba!</h3>
                     <p>Cadastro efetuado com sucesso.</p>
@@ -60,9 +81,10 @@ function saveData(formJSON) {
                 </form>
             `;
         })
-        .fail((error) => { // Se falhou, mostra feeback.
+        .fail((error) => {
+            // Se falhar, mostra uma mensagem de erro no console.
             console.error('Erro:', error.status, error.statusText, error.responseJSON);
-            viewHTML = `
+            var viewHTML = `
                 <form>
                     <h3>Oooops!</h3>
                     <p>Não foi possível realizar o cadastro. Ocorreu uma falha no servidor.</p>
@@ -70,7 +92,9 @@ function saveData(formJSON) {
             `;
         })
         .always(() => {
+            // Atualiza o conteúdo da guia com o resultado da solicitação.
             $('.tabBlock').html(viewHTML);
+            // Reseta os formulários de dono (owner) e item.
             $('#formNewOwner').trigger('reset');
             $('#formNewItem').trigger('reset');
         });
@@ -78,11 +102,13 @@ function saveData(formJSON) {
     return false;
 }
 
+// Função para alternar entre as guias de dono (owner) e item.
 function showTab(tabName) {
-
+    // Reseta os formulários de dono (owner) e item.
     $('#formNewOwner').trigger('reset');
     $('#formNewItem').trigger('reset');
 
+    // Exibe a guia correspondente com base no nome fornecido.
     switch (tabName) {
         case 'owner':
             $('#tabOwner').show();
@@ -98,26 +124,29 @@ function showTab(tabName) {
             $('#btnNewOwner').attr('class', 'inactive');
             break;
     }
-
 }
 
+// Função para obter opções de dono (owner) para preenchimento do menu suspenso.
 function getOwnersToSelect() {
+    // Constrói a URL da solicitação para obter donos (owners).
+    var requestURL = `${app.apiBaseURL}/owners`;
 
-    requestURL = `${app.apiBaseURL}/owners`;
-
+    // Faz uma solicitação GET usando jQuery para obter dados de donos (owners).
     $.get(requestURL)
         .done((apiData) => {
-
+            // Processa os dados obtidos e adiciona opções ao menu suspenso de dono (owner).
             apiData.forEach((item) => {
                 ownerOptions += `<option value="${item.id}">${item.id} - ${item.name}</option>`;
             });
 
+            // Atualiza o conteúdo do menu suspenso de dono (owner) no formulário.
             $('#owner').html(ownerOptions);
         })
         .fail((error) => {
+            // Em caso de falha, exibe uma mensagem de erro no console.
             console.error('Erro:', error.status, error.statusText, error.responseJSON);
         });
-
 }
 
+// Executa a função myHome quando o documento estiver pronto.
 $(document).ready(myHome);
